a9c18fa4ed263a275972a4a9541a37b5c4bc294d
vamsi824 <158843121+vamsi824@users.noreply.github.com>
2024-04-30 22:21:52 +0530

    Create demo.java

---
+++ week10/demo.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.applet.*;
+
+/*
+<applet code="Demo.class" width="1000" height="1000"> </applet>
+*/
+public class Demo extends Applet {
+	
+	public void paint(Graphics g) {
+		g.drawLine(10,10,300,300);
+	}
+}
